b0c0de1da0a865e4418ae6252a03c53e8922d088
Jordi Farré <jordi.farre@mango.com>
2017-08-01 21:01:50 +0200

    generated golden master for 50 days

---
+++ Java/src/test/java/com/gildedrose/GoldenMasterGenerator.java
@@ -0,0 +1,16 @@
+package com.gildedrose;
+
+import java.io.*;
+
+public class GoldenMasterGenerator {
+
+    public static void main(String[] args) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("src/test/java/com/gildedrose/golden-master.txt", true);
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+             PrintStream printStream = new PrintStream(bufferedOutputStream)) {
+             System.setOut(printStream);
+             TexttestFixture.main(new String[]{"50"});
+        }
+    }
+
+}
