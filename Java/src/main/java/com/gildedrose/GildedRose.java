22244e63e94c645f64529fe0c25125396af292f2
Jordi Farré <jordi.farre@mango.com>
2017-08-02 09:50:24 +0200

    extracted remaining increase quality ocurrence to method calling

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,17 +15,15 @@ class GildedRose {
                     decreaseQuality(item);
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                increaseQuality(item);
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            increaseQuality(item);
-                        }
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        increaseQuality(item);
+                    }
 
-                        if (item.sellIn < 6) {
-                            increaseQuality(item);
-                        }
+                    if (item.sellIn < 6) {
+                        increaseQuality(item);
                     }
                 }
             }
