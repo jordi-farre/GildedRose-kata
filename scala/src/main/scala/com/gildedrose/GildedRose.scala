d3eddd38ce0976fbc0d21863d602b35271e7bead
Jordi Farré <jordi.farre@mango.com>
2017-08-03 00:14:24 +0200

    merged both conditional groups

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -8,6 +8,9 @@ class GildedRose(val items: Array[Item]) {
       decreaseSellIn(item)
       if (item.name.equals("Aged Brie")) {
         increaseQuality(item)
+        if (item.sellIn < 0) {
+          increaseQuality(item)
+        }
       } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         increaseQuality(item)
         if (item.sellIn < 10) {
@@ -16,24 +19,14 @@ class GildedRose(val items: Array[Item]) {
         if (item.sellIn < 5) {
           increaseQuality(item)
         }
-      } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-      } else {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
-      }
-
-
-      if (item.name.equals("Aged Brie")) {
-        if (item.sellIn < 0) {
-          increaseQuality(item)
-        }
-      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (item.sellIn < 0) {
           item.quality = 0
         }
       } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
       } else {
+        if (item.quality > 0) {
+          item.quality = item.quality - 1
+        }
         if (item.quality > 0 && item.sellIn < 0) {
           item.quality = item.quality - 1
         }
