146ccdb2d5a453c4e1a2698313aa9db651d50d6d
Jordi Farré <jordi.farre@mango.com>
2017-08-02 23:46:31 +0200

    added test for golden master

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -1,12 +1,13 @@
 package com.gildedrose
 
+import java.nio.file.{Files, Paths}
+
 import org.scalatest._
 
 class GildedRoseTest  extends FlatSpec with Matchers {
       it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
+        val expected = new String(Files.readAllBytes(Paths.get(this.getClass.getResource("golden-master.txt").toURI)))
+        val result = new GildedRoseExecutor().execute(50)
+        (expected) should equal (result)
       }
 }
