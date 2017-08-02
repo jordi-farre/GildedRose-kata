4dd2b0081b971b11e4f03a76965d31a696e3d821
Jordi Farré <jordi.farre@mango.com>
2017-08-03 00:19:29 +0200

    changed if/else to match condition

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -6,29 +6,33 @@ class GildedRose(val items: Array[Item]) {
   def updateQuality() {
     items.foreach { item =>
       decreaseSellIn(item)
-      if (item.name.equals("Aged Brie")) {
-        increaseQuality(item)
-        if (item.sellIn < 0) {
+      item.name match {
+        case "Aged Brie" => {
           increaseQuality(item)
+          if (item.sellIn < 0) {
+            increaseQuality(item)
+          }
         }
-      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        increaseQuality(item)
-        if (item.sellIn < 10) {
+        case "Backstage passes to a TAFKAL80ETC concert" => {
           increaseQuality(item)
+          if (item.sellIn < 10) {
+            increaseQuality(item)
+          }
+          if (item.sellIn < 5) {
+            increaseQuality(item)
+          }
+          if (item.sellIn < 0) {
+            item.quality = 0
+          }
         }
-        if (item.sellIn < 5) {
-          increaseQuality(item)
-        }
-        if (item.sellIn < 0) {
-          item.quality = 0
-        }
-      } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-      } else {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
-        if (item.quality > 0 && item.sellIn < 0) {
-          item.quality = item.quality - 1
+        case "Sulfuras, Hand of Ragnaros" => {}
+        case _ => {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
+          }
+          if (item.quality > 0 && item.sellIn < 0) {
+            item.quality = item.quality - 1
+          }
         }
       }
     }
