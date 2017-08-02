d6459f7ae14595ed11c8923e05a6b5c8d9c00248
Jordi Farré <jordi.farre@mango.com>
2017-08-03 00:10:51 +0200

    moved sellin verification inside every conditional

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -24,16 +24,18 @@ class GildedRose(val items: Array[Item]) {
       }
 
 
-      if (item.sellIn < 0) {
-        if (item.name.equals("Aged Brie")) {
+      if (item.name.equals("Aged Brie")) {
+        if (item.sellIn < 0) {
           increaseQuality(item)
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        }
+      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (item.sellIn < 0) {
           item.quality = 0
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1
-          }
+        }
+      } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+      } else {
+        if (item.quality > 0 && item.sellIn < 0) {
+          item.quality = item.quality - 1
         }
       }
     }
